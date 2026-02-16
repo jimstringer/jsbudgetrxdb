1ea6a565c7803f439af585c53daaf07fe839c27c
James Stringer <jim.stringer@gmail.com>
2026-02-16 16:48:15 -0600

    fix lint error. removed unused code

--- src/database/sync/syncClient.ts
+++ src/database/sync/syncClient.ts
@@ -1,17 +1,10 @@
 // > Sync to remote server when I know I'm online
 // The server will run on my labtop at port 4000 and my phone will connect to it
-import type { JsBudgetDatabase } from '../db';
+//import type { JsBudgetDatabase } from '../db';
 
 
 export const syncClient = () => {
 
-
-    
-  return (
-    <div>
-      
-    </div>
-  )
 };
 
 
