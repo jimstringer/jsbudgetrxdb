bb24dbbf5969188557238e59595b69c063b47a8e
James Stringer <jim.stringer@gmail.com>
2026-02-17 09:07:09 -0600

    Use input file to import firebase backup.

--- src/pages/backup/ImportFB.tsx
+++ src/pages/backup/ImportFB.tsx
@@ -1,5 +1,9 @@
 // Import from firebase backup file
-import Expenses from "../../../local/expenses.json";
+// import { Expenses } from "../../../local/expenses.json";
+// I don't want to put this file in git so need to load it from file
+//import Expenses from "../../../local/expenses.json";
+
+//TODO: get the backup from the server using axios
 
 import { uuidv7 } from "uuidv7";
 import type {
@@ -9,7 +13,6 @@ import type {
 import useRxDB from "../../hooks/useRxDB";
 import { useState } from "react";
 
-/* 
 interface Expense {
   docid: string;
   mydate: number;
@@ -19,7 +22,6 @@ interface Expense {
   Income: boolean;
   Comment: string;
 }
- */
 
 /* function yearfrommydate(mydate: number) {
   return +mydate.toString().substring(0, 4);
@@ -28,14 +30,29 @@ function yearmonth(mydate: number) {
   return +mydate.toString().substring(0, 6);
 } */
 
+const readJsonFile = (file: Blob) =>
+  new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+
+    fileReader.onload = (event) => {
+      if (event.target) {
+        resolve(JSON.parse(event.target.result as string));
+      }
+    };
+
+    fileReader.onerror = (error) => reject(error);
+    fileReader.readAsText(file);
+  });
+
 export const ImportFB = () => {
   const [importing, setImporting] = useState(false);
+  const [fsexpenses, setFsexpenses] = useState<Expense[]>([]);
 
   const dbctx = useRxDB();
   const db = dbctx.db;
 
   if (!db) {
-    console.warn("In ImportFBDatabase is not initialized");
+    console.warn("In ImportFB Database is not initialized");
     return;
   }
 
@@ -63,7 +80,7 @@ export const ImportFB = () => {
       incomeSourcesMap.set(incomeSource.name.toUpperCase(), incomeSource.name);
     });
 
-    for (const expense of Expenses) {
+    for (const expense of fsexpenses) {
       console.log("Importing expense:", expense);
       console.log("amount:", expense.Amount * 100);
       const forfrom = (comment: string) => {
@@ -104,17 +121,48 @@ export const ImportFB = () => {
       }
     }
     setImporting(false);
+    setFsexpenses([]);
+  };
+
+  const onChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const parsedData = await readJsonFile(event.target.files[0]);
+      //should be an array of Expenses, How to check?
+      setFsexpenses(parsedData as Expense[]); //I say it will be an Expense[]
+      console.log(parsedData);
+    }
   };
 
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Import Firebase Backup</h1>
       <p>This feature allows you to import a backup file from Firebase.</p>
+      <div className="flex w-full h-screen items-center justify-center bg-grey-lighter">
+        <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
+          <svg
+            className="w-8 h-8"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+          </svg>
+          <span className="mt-2 text-base leading-normal">Select firestore backup file</span>
+          <input
+            type="file"
+            accept=".json,application/json"
+            onChange={onChange}
+            className="hidden"
+          />
+        </label>
+      </div>
       {/* Implementation of import functionality goes here */}
       {importing && <p className="text-red-500">Importing...</p>}
       <button
         onClick={saveTodexie}
-        className="border-2 border-solid bg-amber-500"
+        className={`border-2 border-solid bg-amber-500 ${
+          (fsexpenses.length > 0) ? "opacity-50 cursor-not-allowed" : ""
+        }`}
       >
         Save to Dexie
       </button>
