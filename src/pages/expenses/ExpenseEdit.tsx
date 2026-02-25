778b023e2808b6d5e5a837f7391abdb04a54c27a
James Stringer <jim.stringer@gmail.com>
2026-02-25 16:05:14 -0600

    inputMode=decimal, 4th times the charm

--- src/pages/expenses/ExpenseEdit.tsx
+++ src/pages/expenses/ExpenseEdit.tsx
@@ -120,7 +120,7 @@ export default function ExpenseEdit() {
             },
           })}
           type="text"
-          inputMode="numeric"
+          inputMode="decimal"
           placeholder="Amount"
           className="form-input px-4 py-3 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         />
